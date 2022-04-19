019028de3c93389554d7a81baadfc9aa530bd09f
Kirubel Moges <kirubel@Kirubels-MacBook-Pro.local>
2022-04-19 17:13:20 -0500

    Added host ip

--- frontend/src/cloud.js
+++ frontend/src/cloud.js
@@ -1,3 +1,3 @@
-export const isHostedOnCloud = false;
-export const hostedIpAddress = '';
+export const isHostedOnCloud = true;
+export const hostedIpAddress = 'https://mustang-go-7ayuqtevua-uc.a.run.app/';
 export const localIpAddress = 'http://localhost:8080';
