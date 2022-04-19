62871a71f0cb1c3f593571d3265da5f12b448040
Kirubel Moges <kirubel@Kirubels-MacBook-Pro.local>
2022-04-19 17:40:52 -0500

    host ip & on path deployments

--- frontend/src/cloud.js
+++ frontend/src/cloud.js
@@ -1,3 +1,3 @@
 export const isHostedOnCloud = true;
-export const hostedIpAddress = 'https://mustang-go-7ayuqtevua-uc.a.run.app/';
+export const hostedIpAddress = 'https://backendimage-7ayuqtevua-uc.a.run.app';
 export const localIpAddress = 'http://localhost:8080';
